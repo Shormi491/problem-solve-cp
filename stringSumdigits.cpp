7e6d7d6091f2d9a8f7c457a0226795e72ae45f01
Shormi Rani Nath <shormirnath@gmail.com>
2025-12-04 19:12:50 +0600

    Create stringSumdigits.cpp

---
+++ stringSumdigits.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int N;
+    cin>>N;
+    string A;
+    cin>>A;
+    int sum =0;
+    for(int i=0;i<N;i++){
+        sum+=A[i] - '0';
+    }
+    cout<<sum<<endl;
+ 
+ 
+ 
+return 0;
+}
