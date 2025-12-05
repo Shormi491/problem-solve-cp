76d595bcdc6a295d9e91251c233cf00822a9730b
Shormi Rani Nath <shormirnath@gmail.com>
2025-12-05 20:19:52 +0600

    Create  Max and Min.cpp

---
+++ Max and Min.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int A,B,C;
+    cin>>A>>B>>C;
+    int mn = min({A,B,C});
+    int mx = max({A,B,C});
+    cout<< mn <<" "<< mx <<endl;
+    return 0;
+
+
+}
