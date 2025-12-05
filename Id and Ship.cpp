a6f33991bca28ea461ff9729299a3afd2109b00e
Shormi Rani Nath <shormirnath@gmail.com>
2025-12-05 20:21:38 +0600

    Create Id and Ship.cpp

---
+++ Id and Ship.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    for(int i =0;i<T;i++){
+        char ch;
+        cin>>ch;
+        if(ch=='B' || ch=='b'){
+            cout<< "BattleShip" <<endl;
+        }
+        else if
+            (ch=='C' || ch=='c'){
+            cout<< "Cruiser" << endl;
+        }
+        else if
+            (ch=='D' || ch=='d'){
+
+            cout<< "Destroyer" << endl;
+        }
+        else if
+            (ch=='F' || ch=='f'){
+
+            cout<< "Frigate" << endl;
+        }
+
+
+    }
+    return 0;
+
+
+}
