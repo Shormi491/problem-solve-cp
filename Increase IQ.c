24d3dc4c38ccae87d23dca2406b6266cf37b71a3
Shormi Rani Nath <shormirnath@gmail.com>
2025-12-05 20:01:38 +0600

    Create Increase IQ.c

---
+++ Increase IQ.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int X;
+    scanf("%d",&X);
+    if(X+7>170){
+        printf("Yes\n");
+    }
+
+    else{
+        printf("No\n");
+    }
+
+    return 0;
+
+}
