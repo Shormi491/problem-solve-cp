c84101cee5b6844b6a1f8c87710b48f461516a67
Shormi Rani Nath <shormirnath@gmail.com>
2025-12-05 20:27:39 +0600

    Create Chef And Operators.cpp

---
+++ Chef And Operators.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+
+    for(int i=0; i<T; i++){
+        long long A,B;
+        cin>>A>>B;
+        if(A>B){
+            cout<< ">" << endl;
+        }
+        else if(A<B){
+                cout<< "<" <<endl;
+
+        }
+        else{
+            cout<< "="<<endl;
+        }
+
+    }
+
+   return 0;
+}
