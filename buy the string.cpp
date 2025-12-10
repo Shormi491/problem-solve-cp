82259cfedfa18b3b889748b208529b0ee38e7c81
Shormi Rani Nath <shormirnath@gmail.com>
2025-12-10 18:22:22 +0600

    Create buy the string.cpp

---
+++ buy the string.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve(){
+    int n, c0, c1, h; cin >> n >> c0 >> c1 >> h;
+    string s; cin >> s;
+    int total(0);
+    for(int i=0; i<n; i++){
+        if(s[i] == '0'){
+            total += min(c0, c1 + h);
+        }
+        else total += min(c1, c0 + h);
+    }
+    cout << total << endl;
+
+}
+     
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
