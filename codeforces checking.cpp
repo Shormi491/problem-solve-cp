9db9fdbbaf8ab2cb484039ae93d020850fda900d
Shormi Rani Nath <shormirnath@gmail.com>
2025-12-10 17:54:40 +0600

    Create codeforces checking.cpp

---
+++ codeforces checking.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void check(string s, char c){
+    for(char i : s){
+        if(i == c){
+            cout << "YES" << endl;
+            return;
+        }
+    }
+    cout << "NO" << endl;
+}
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        string s = "codefrs";
+        char c;
+        cin >> c;
+        check(s, c);
+    }
+
+    return 0;
+}
