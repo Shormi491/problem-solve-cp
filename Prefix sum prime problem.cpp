47696532ce0170f00650938783b643dcf6e32a77
Shormi Rani Nath <shormirnath@gmail.com>
2025-12-10 18:15:29 +0600

    Create Prefix sum prime problem.cpp

---
+++ Prefix sum prime problem.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+
+int main(){
+    int n; cin >> n;
+    int ones(0), twos(0);
+    vector<int> a(n);
+    for(int &i : a){
+        cin >> i;
+        if(i == 1) ones++;
+        else twos++;
+    }
+    if(ones == 0){
+        for(int i=0; i<twos; i++){
+            cout << 2 << " ";
+        }
+        cout << "\n";
+    } 
+    else if(twos == 0){
+        for(int i=0; i<ones; i++){
+            cout << 1 << " ";
+        }
+        cout << "\n";
+    }
+    else{
+        cout << "2 1 ";
+        for(int i=0; i<twos-1; i++){
+            cout << 2 << " ";
+        }
+        for(int i=0; i<ones-1; i++){
+            cout << 1 << " ";
+        }
+        cout << "\n";
+    }
+}
