423b473c34f381fb988136e9feb2fe89b018a241
Shormi Rani Nath <shormirnath@gmail.com>
2025-12-10 18:36:31 +0600

    Create AB Balance.cpp

---
+++ AB Balance.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+
+void solve(){
+    string s; cin >> s;
+    if(s[0] == s[s.size()-1]){
+        cout << s << endl;
+    }else{
+        s[0] = s[s.size() - 1];
+        cout << s << endl;
+    }
+}
+     
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
