b76f83c982ba3f565d122439b557deaad7b63bb5
Shormi Rani Nath <shormirnath@gmail.com>
2025-12-10 18:48:06 +0600

    Create Grasshopper on a Line.cpp

---
+++ Grasshopper on a Line.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve(){
+    int x, k; cin >> x >> k;
+
+    if(x % k != 0){
+        cout << 1 << endl;
+        cout << x << endl;
+        return;
+    }
+    int n = 0;
+    for(int i=1; i<x; i++){
+        if(i % k !=0){
+            if(i > n) n = i;
+        }
+    }
+    cout << 2 << endl;
+    cout << n << " " << x - n << endl;
+
+}
+
+int main(){
+    ios :: sync_with_stdio(false);
+    cin.tie(0);
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
