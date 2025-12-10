ba29ef3b7775aed24832a20d70ce2ebd63011634
Shormi Rani Nath <shormirnath@gmail.com>
2025-12-10 18:51:59 +0600

    Create Array coloring.cpp

---
+++ Array coloring.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve(){
+    int n; cin >> n;
+    int e(0), o(0);
+    vector<int> v(n);
+    for(int i=0; i<n; i++){
+        cin >> v[i];
+        if(v[i]&1) o++;
+        else e++;
+    }
+    if(o&1) cout << "NO\n";
+    else cout << "YES\n";
+}
+
+
+int main(){
+    ios :: sync_with_stdio(false);
+    cin.tie(0);
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+
+}
