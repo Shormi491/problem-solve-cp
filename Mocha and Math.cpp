854d5355e61d45e0284d48c7f2ae92e3ecac35ac
Shormi Rani Nath <shormirnath@gmail.com>
2025-12-10 18:32:55 +0600

    Create Mocha and Math.cpp

---
+++ Mocha and Math.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve(){
+    int n; cin >> n;
+    vector<int> a(n);
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+    }
+    int x = a[0];
+    for(int i=0; i<n; i++){
+       x &= a[i];
+    }
+    cout << x << endl; 
+
+}
+     
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
