e29093f3f0edc1852ff8d01898ad34694c91042c
Shormi Rani Nath <shormirnath@gmail.com>
2025-12-10 18:40:45 +0600

    Create AvtoBus.cpp

---
+++ AvtoBus.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+
+void solve(){ 
+    ll n; cin >> n;
+    if(n < 4 || n % 2 != 0){
+        cout << -1 << endl;
+        return;
+    }
+    ll mn = (n+5)/6;
+    ll mx = n / 4;
+    cout << mn << " " << mx << endl;
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
